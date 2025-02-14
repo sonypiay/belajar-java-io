4f1e628cdaf9c5f1834cd6f1036898b4ee56129b
sonypiay <pyscho30@gmail.com>
2025-02-15 01:53:03 +0700

    init project

---
+++ src/main/java/programmer/zaman/now/belajar_java_io/BelajarJavaIoApplication.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.belajar_java_io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarJavaIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarJavaIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmer/zaman/now/belajar_java_io/BelajarJavaIoApplicationTests.java
@@ -0,0 +1,13 @@
+package programmer.zaman.now.belajar_java_io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
